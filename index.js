b5b786b2d43b76a25aa6d4b454151c5deb90d70e
Martin Häger <martin.hager@minmb.se>
2013-03-19 11:57:02 +0100

    extract compilers into a separate module, do not add these by default

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var AssetsCompiler = require('./lib/assets-compiler');
 
+exports.compilers = require('./lib/compilers');
+
 exports.init = function(compound) {
     compound.assetsCompiler = new AssetsCompiler(compound);
     compound.injectMiddlewareAfter('expressInit', compound.assetsCompiler.init());
